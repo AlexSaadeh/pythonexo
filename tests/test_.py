bc776c89950fa91040cc5ea87dd3dec5c1d6e59d
AlexSaadeh <alexsaade.lb@hotmail.com>
2024-09-30 11:27:37 +0200

    Fix function names and update tests

--- tests/test_.py
+++ tests/test_.py
@@ -1,28 +1,24 @@
-# tests/test_.py
+import pytest
+from python import identity_function, mult_two, rectangle_perimeter, is_even, determine_sign, find_remainder
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from python import func, mult_two, rectangle_perimeter, is_even, determine_sign, find_remainder
-
-def test_func():
-    assert func(3) == 3
-    assert func("string") == "string"
-    assert func(True) == True
+def test_identity_function():
+    assert identity_function(3) == 3
+    assert identity_function("string") == "string"
+    assert identity_function(True) is True
 
 def test_mult_two():
-    assert mult_two(1, 2) == 2
     assert mult_two(3, 2) == 6
     assert mult_two(0, 1) == 0
 
 def test_rectangle_perimeter():
     assert rectangle_perimeter(2, 4) == 12
     assert rectangle_perimeter(3, 5) == 16
+    assert rectangle_perimeter(10, 20) == 60
 
 def test_is_even():
-    assert is_even(2) == True
-    assert is_even(5) == False
+    assert is_even(2) is True
+    assert is_even(5) is False
+    assert is_even(0) is True
 
 def test_determine_sign():
     assert determine_sign(5) == "positive"
